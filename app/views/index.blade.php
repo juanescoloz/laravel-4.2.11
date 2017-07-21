e0026b5b375775be94522af3426982658b2e68b5
Juan Carlos Escobar Lozada <jcescobar04@misena.edu.co>
2017-07-21 17:13:06 -0500

    Se crea modulo proveedores

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -88,14 +88,10 @@
                       <li><a href="{{url('ListarUsuario')}}">Visualizar Usuarios</a></li>
                     </ul>
                   </li>
-                  <li><a><i class="fa fa-edit"></i> Formas <span class="fa fa-chevron-down"></span></a>
+                  <li><a><i class="fa fa-edit"></i> Proveedores <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="form.html">General Form</a></li>
-                      <li><a href="form_advanced.html">Advanced Components</a></li>
-                      <li><a href="form_validation.html">Form Validation</a></li>
-                      <li><a href="form_wizards.html">Form Wizard</a></li>
-                      <li><a href="form_upload.html">Form Upload</a></li>
-                      <li><a href="form_buttons.html">Form Buttons</a></li>
+                      <li><a href="form.html">Crear Proveedor</a></li>
+                      <li><a href="form_advanced.html">Visualizar Proveedores</a></li>
                     </ul>
                   </li>
                   <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
